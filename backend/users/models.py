c05cc982580a76fb2f1e9394580696b368ebfa1c
NicholasCF <nicholas.fernando@u.nus.edu>
2021-03-16 17:09:14 +0800

    Fix indentation

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-  # Enforce email uniqueness
-  email = models.EmailField(_('email address'), unique=True, blank=True)
+    # Enforce email uniqueness
+    email = models.EmailField(_('email address'), unique=True, blank=True)
