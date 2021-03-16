cbd784bcf91822fd45f36adb1595eef25c59e793
NicholasCF <nicholas.fernando@u.nus.edu>
2021-03-16 17:56:10 +0800

    Add roles to users

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+# Custom admin to allow edit of roles in admin
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+            ('Extra Fields', {'fields': ('role',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
